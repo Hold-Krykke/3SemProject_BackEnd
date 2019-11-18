1d14983e1a1deae89db013d3fe85b4999ee96473
Rúni VN <Runiniclasen@hotmail.com>
2019-11-18 13:11:39 +0100

    Initial cleanup

--- src/main/java/facades/FacadeExample.java
+++ src/main/java/facades/FacadeExample.java
@@ -1,11 +1,5 @@
 package facades;
 
-import entities.RenameMe;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 /**
  *
  * Rename Class to a relevant name Add add relevant facade methods
@@ -13,39 +7,26 @@ import javax.persistence.Persistence;
 public class FacadeExample {
 
     private static FacadeExample instance;
-    private static EntityManagerFactory emf;
-    
+
     //Private Constructor to ensure Singleton
-    private FacadeExample() {}
-    
-    
+    private FacadeExample() {
+    }
+
     /**
-     * 
-     * @param _emf
+     *
+
      * @return an instance of this facade class.
      */
-    public static FacadeExample getFacadeExample(EntityManagerFactory _emf) {
+    public static FacadeExample getFacadeExample() {
         if (instance == null) {
-            emf = _emf;
             instance = new FacadeExample();
         }
         return instance;
     }
 
-    private EntityManager getEntityManager() {
-        return emf.createEntityManager();
-    }
-    
     //TODO Remove/Change this before use
-    public long getRenameMeCount(){
-        EntityManager em = emf.createEntityManager();
-        try{
-            long renameMeCount = (long)em.createQuery("SELECT COUNT(r) FROM RenameMe r").getSingleResult();
-            return renameMeCount;
-        }finally{  
-            em.close();
-        }
-        
+    public String getFacadeMessage() {
+        return "Hello from the facade";
     }
 
 }
--- src/test/java/rest/RenameMeResourceTest.java
+++ src/test/java/rest/RenameMeResourceTest.java
@@ -1,12 +1,9 @@
 package rest;
 
-import entities.RenameMe;
-import utils.EMF_Creator;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
 import io.restassured.parsing.Parser;
 import java.net.URI;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.core.UriBuilder;
 import org.glassfish.grizzly.http.server.HttpServer;
@@ -17,18 +14,14 @@ import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import utils.EMF_Creator.DbSelector;
-import utils.EMF_Creator.Strategy;
 
-//Uncomment the line below, to temporarily disable this test
+
 //@Disabled
 public class RenameMeResourceTest {
 
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/api";
-    private static RenameMe r1,r2;
     
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
@@ -41,10 +34,6 @@ public class RenameMeResourceTest {
 
     @BeforeAll
     public static void setUpClass() {
-        //This method must be called before you request the EntityManagerFactory
-        EMF_Creator.startREST_TestWithDB();
-        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.CREATE);
-        
         httpServer = startServer();
         //Setup RestAssured
         RestAssured.baseURI = SERVER_URL;
@@ -54,54 +43,36 @@ public class RenameMeResourceTest {
     
     @AfterAll
     public static void closeTestServer(){
-        //System.in.read();
-         //Don't forget this, if you called its counterpart in @BeforeAll
-         EMF_Creator.endREST_TestWithDB();
          httpServer.shutdownNow();
     }
     
-    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
-    //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
     @BeforeEach
     public void setUp() {
-        EntityManager em = emf.createEntityManager();
-        r1 = new RenameMe("Some txt","More text");
-        r2 = new RenameMe("aaa","bbb");
-        try {
-            em.getTransaction().begin();
-            em.createNamedQuery("RenameMe.deleteAllRows").executeUpdate();
-            em.persist(r1);
-            em.persist(r2); 
-            em.getTransaction().commit();
-        } finally { 
-            em.close();
-        }
     }
     
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
-        given().when().get("/xxx").then().statusCode(200);
+        given().when().get("/resource").then().statusCode(200);
     }
    
-    //This test assumes the database contains two rows
     @Test
     public void testDummyMsg() throws Exception {
         given()
         .contentType("application/json")
-        .get("/xxx/").then()
+        .get("/resource/").then()
         .assertThat()
         .statusCode(HttpStatus.OK_200.getStatusCode())
         .body("msg", equalTo("Hello World"));   
     }
     
     @Test
-    public void testCount() throws Exception {
+    public void testFacadeMessage() throws Exception {
         given()
         .contentType("application/json")
-        .get("/xxx/count").then()
+        .get("/resource/facade").then()
         .assertThat()
         .statusCode(HttpStatus.OK_200.getStatusCode())
-        .body("count", equalTo(2));   
+        .body("facadeMessage", equalTo("Hello from the facade"));   
     }
 }

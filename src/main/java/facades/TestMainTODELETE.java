5bed4444b05f83e0432101a49e155a802a906b4d
Camilla <camilla.valerius@gmail.com>
2019-11-20 23:28:33 +0100

    changed todeletetest

--- src/main/java/facades/TestMainTODELETE.java
+++ src/main/java/facades/TestMainTODELETE.java
@@ -7,10 +7,10 @@ import com.google.gson.Gson;
  * @author Camilla
  */
 public class TestMainTODELETE {
+    private static final EventFacade FACADE = EventFacade.getEventFacade();
     public static void main(String[] args) throws Exception {
-        EventFacade facade = new EventFacade();
         Gson gson = new Gson();
-        System.out.println(facade.getApiData(55.681554, 12.591135, "2019-11-23T00:00:00Z", "2019-11-25T23:59:59Z" ));
+        System.out.println(FACADE.getApiData(55.681554, 12.591135, "2019-11-23T00:00:00Z", "2019-11-25T23:59:59Z" ));
         
         // https://app.ticketmaster.com/discovery/v2/events.json?
         // (Double latitude, Double longitude, String url, String startdate, String enddate)

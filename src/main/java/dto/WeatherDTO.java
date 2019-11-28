a316e7b6b9e415b6c16968d7b29e74ed42b32298
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-28 16:36:40 +0100

    Changed DTO to fit data.

--- src/main/java/dto/WeatherDTO.java
+++ src/main/java/dto/WeatherDTO.java
@@ -10,69 +10,158 @@ package dto;
  * @author Malte
  */
 public class WeatherDTO {
-
-    private String city;
-    private String year;
-    private String month;
-    private String day;
+    
+    /*
+    {
+    "dateTime":"10.10.2016 10:20",
+    "funnyAdvice":"An umbrella is not going to be enough, take a cap or drown in the rain",
+    "humidity":91,
+    "predictability":77,
+    "temp":10.375,
+    "weatherIcon":"https://www.metaweather.com/static/img/weather/png/hr.png",
+    "weatherStatus":"Heavy Rain",
+    "windDirection":"NE",
+    "windSpeed":13.072619799015087}
+    */
+    
+    private String dateTime;
+    private String funnyAdvice;
+    private String weatherIcon;
+    private String weatherStatus;
+    private String windDirection;
+    private int humidity;
+    private int predictability;
+    private double temp;
+    private double windSpeed;
 
     public WeatherDTO() {
     }
-
+    
     /**
-     * @return the city
+     * @return the dateTime
      */
-    public String getCity() {
-        return city;
+    public String getDateTime() {
+        return dateTime;
     }
 
     /**
-     * @param city the city to set
+     * @param dateTime the dateTime to set
      */
-    public void setCity(String city) {
-        this.city = city;
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
     }
 
     /**
-     * @return the year
+     * @return the funnyAdvice
      */
-    public String getYear() {
-        return year;
+    public String getFunnyAdvice() {
+        return funnyAdvice;
     }
 
     /**
-     * @param year the year to set
+     * @param funnyAdvice the funnyAdvice to set
      */
-    public void setYear(String year) {
-        this.year = year;
+    public void setFunnyAdvice(String funnyAdvice) {
+        this.funnyAdvice = funnyAdvice;
     }
 
     /**
-     * @return the month
+     * @return the weatherIcon
      */
-    public String getMonth() {
-        return month;
+    public String getWeatherIcon() {
+        return weatherIcon;
     }
 
     /**
-     * @param month the month to set
+     * @param weatherIcon the weatherIcon to set
      */
-    public void setMonth(String month) {
-        this.month = month;
+    public void setWeatherIcon(String weatherIcon) {
+        this.weatherIcon = weatherIcon;
     }
 
     /**
-     * @return the day
+     * @return the weatherStatus
      */
-    public String getDay() {
-        return day;
+    public String getWeatherStatus() {
+        return weatherStatus;
     }
 
     /**
-     * @param day the day to set
+     * @param weatherStatus the weatherStatus to set
      */
-    public void setDay(String day) {
-        this.day = day;
+    public void setWeatherStatus(String weatherStatus) {
+        this.weatherStatus = weatherStatus;
     }
+
+    /**
+     * @return the windDirection
+     */
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    /**
+     * @param windDirection the windDirection to set
+     */
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    /**
+     * @return the humidity
+     */
+    public int getHumidity() {
+        return humidity;
+    }
+
+    /**
+     * @param humidity the humidity to set
+     */
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    /**
+     * @return the predictability
+     */
+    public int getPredictability() {
+        return predictability;
+    }
+
+    /**
+     * @param predictability the predictability to set
+     */
+    public void setPredictability(int predictability) {
+        this.predictability = predictability;
+    }
+
+    /**
+     * @return the temp
+     */
+    public double getTemp() {
+        return temp;
+    }
+
+    /**
+     * @param temp the temp to set
+     */
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    /**
+     * @return the windSpeed
+     */
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    /**
+     * @param windSpeed the windSpeed to set
+     */
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+    
 
 }

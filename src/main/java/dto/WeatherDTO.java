ff1d8a37f244616256246f9f7a042c7c2b86f61d
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-28 16:53:30 +0100

    Both good and bad test green.

--- src/main/java/dto/WeatherDTO.java
+++ src/main/java/dto/WeatherDTO.java
@@ -5,12 +5,14 @@
  */
 package dto;
 
+import java.util.Objects;
+
 /**
  *
  * @author Malte
  */
 public class WeatherDTO {
-    
+
     /*
     {
     "dateTime":"10.10.2016 10:20",
@@ -22,8 +24,7 @@ public class WeatherDTO {
     "weatherStatus":"Heavy Rain",
     "windDirection":"NE",
     "windSpeed":13.072619799015087}
-    */
-    
+     */
     private String dateTime;
     private String funnyAdvice;
     private String weatherIcon;
@@ -36,7 +37,32 @@ public class WeatherDTO {
 
     public WeatherDTO() {
     }
-    
+
+    /**
+     * Make a new WeatherDTO.
+     *
+     * @param dateTime
+     * @param funnyAdvice
+     * @param weatherIcon
+     * @param weatherStatus
+     * @param windDirection
+     * @param humidity
+     * @param predictability
+     * @param temp
+     * @param windSpeed
+     */
+    public WeatherDTO(String dateTime, String funnyAdvice, String weatherIcon, String weatherStatus, String windDirection, int humidity, int predictability, double temp, double windSpeed) {
+        this.dateTime = dateTime;
+        this.funnyAdvice = funnyAdvice;
+        this.weatherIcon = weatherIcon;
+        this.weatherStatus = weatherStatus;
+        this.windDirection = windDirection;
+        this.humidity = humidity;
+        this.predictability = predictability;
+        this.temp = temp;
+        this.windSpeed = windSpeed;
+    }
+
     /**
      * @return the dateTime
      */
@@ -162,6 +188,67 @@ public class WeatherDTO {
     public void setWindSpeed(double windSpeed) {
         this.windSpeed = windSpeed;
     }
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + Objects.hashCode(this.dateTime);
+        hash = 43 * hash + Objects.hashCode(this.funnyAdvice);
+        hash = 43 * hash + Objects.hashCode(this.weatherIcon);
+        hash = 43 * hash + Objects.hashCode(this.weatherStatus);
+        hash = 43 * hash + Objects.hashCode(this.windDirection);
+        hash = 43 * hash + this.humidity;
+        hash = 43 * hash + this.predictability;
+        hash = 43 * hash + (int) (Double.doubleToLongBits(this.temp) ^ (Double.doubleToLongBits(this.temp) >>> 32));
+        hash = 43 * hash + (int) (Double.doubleToLongBits(this.windSpeed) ^ (Double.doubleToLongBits(this.windSpeed) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final WeatherDTO other = (WeatherDTO) obj;
+        if (this.humidity != other.humidity) {
+            return false;
+        }
+        if (this.predictability != other.predictability) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.temp) != Double.doubleToLongBits(other.temp)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.windSpeed) != Double.doubleToLongBits(other.windSpeed)) {
+            return false;
+        }
+        if (!Objects.equals(this.dateTime, other.dateTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.funnyAdvice, other.funnyAdvice)) {
+            return false;
+        }
+        if (!Objects.equals(this.weatherIcon, other.weatherIcon)) {
+            return false;
+        }
+        if (!Objects.equals(this.weatherStatus, other.weatherStatus)) {
+            return false;
+        }
+        if (!Objects.equals(this.windDirection, other.windDirection)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherDTO{" + "dateTime=" + dateTime + ", funnyAdvice=" + funnyAdvice + ", weatherIcon=" + weatherIcon + ", weatherStatus=" + weatherStatus + ", windDirection=" + windDirection + ", humidity=" + humidity + ", predictability=" + predictability + ", temp=" + temp + ", windSpeed=" + windSpeed + '}';
+    }
 
 }
--- src/test/java/facades/WeatherFacadeTest.java
+++ src/test/java/facades/WeatherFacadeTest.java
@@ -5,8 +5,10 @@
  */
 package facades;
 
+import dto.WeatherDTO;
 import errorhandling.NotFoundException;
 import static io.restassured.RestAssured.given;
+import java.util.List;
 import org.glassfish.grizzly.http.util.HttpStatus;
 import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.api.AfterEach;
@@ -24,6 +26,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class WeatherFacadeTest {
 
     private static WeatherFacade facade;
+    
+    private WeatherDTO weatherOne;
+    private WeatherDTO weatherTwo;
 
     public WeatherFacadeTest() {
     }
@@ -37,8 +42,35 @@ public class WeatherFacadeTest {
     public static void tearDownClass() {
     }
 
+    /*
+    [
+    {
+    "dateTime":"04.10.2017 09:22",
+    "funnyAdvice":"Remember your umbrella today!",
+    "humidity":79,
+    "predictability":75,
+    "temp":12.809999999999999,
+    "weatherIcon":"https://www.metaweather.com/static/img/weather/png/lr.png",
+    "weatherStatus":"Light Rain",
+    "windDirection":"WSW",
+    "windSpeed":15.574903598929453
+    },{
+    "dateTime":"04.10.2017 12:22",
+    "funnyAdvice":"An umbrella is not going to be enough, take a cap or drown in the rain",
+    "humidity":83,
+    "predictability":77,
+    "temp":12.29,
+    "weatherIcon":"https://www.metaweather.com/static/img/weather/png/hr.png",
+    "weatherStatus":"Heavy Rain","windDirection":"WSW","windSpeed":15.488408930315757
+    },{
+    "dateTime":"04.10.2017 15:22","funnyAdvice":"Remember your umbrella today!","humidity":83,"predictability":75,"temp":12.3,"weatherIcon":"https://www.metaweather.com/static/img/weather/png/lr.png","weatherStatus":"Light Rain","windDirection":"WSW","windSpeed":15.095879450315755},{"dateTime":"04.10.2017 18:22","funnyAdvice":"Remember your umbrella today!","humidity":83,"predictability":75,"temp":11.21,"weatherIcon":"https://www.metaweather.com/static/img/weather/png/lr.png","weatherStatus":"Light Rain","windDirection":"WSW","windSpeed":15.334485988134892},{"dateTime":"04.10.2017 21:22","funnyAdvice":"Remember your umbrella today!","humidity":84,"predictability":75,"temp":11.21,"weatherIcon":"https://www.metaweather.com/static/img/weather/png/lr.png","weatherStatus":"Light Rain","windDirection":"WSW","windSpeed":14.942485988134894}]
+    */
     @BeforeEach
     public void setUp() {
+        
+        weatherOne = new WeatherDTO("04.10.2017 09:22", "Remember your umbrella today!", "https://www.metaweather.com/static/img/weather/png/lr.png", "Light Rain", "WSW", 79, 75, 12.809999999999999, 15.574903598929453);
+        weatherTwo = new WeatherDTO("04.10.2017 12:22", "An umbrella is not going to be enough, take a cap or drown in the rain", "https://www.metaweather.com/static/img/weather/png/hr.png", "Heavy Rain", "WSW", 83, 77, 12.29, 15.488408930315757);
+        
     }
 
     @AfterEach
@@ -70,6 +102,16 @@ public class WeatherFacadeTest {
         Assertions.assertThrows(NotFoundException.class, () -> {
             facade.getWeather("Copenhagen", "1994", "10", "04");
         });
+    }
+    
+    /*
+    [{"dateTime":"04.10.2017 09:22","funnyAdvice":"Remember your umbrella today!","humidity":79,"predictability":75,"temp":12.809999999999999,"weatherIcon":"https://www.metaweather.com/static/img/weather/png/lr.png","weatherStatus":"Light Rain","windDirection":"WSW","windSpeed":15.574903598929453},{"dateTime":"04.10.2017 12:22","funnyAdvice":"An umbrella is not going to be enough, take a cap or drown in the rain","humidity":83,"predictability":77,"temp":12.29,"weatherIcon":"https://www.metaweather.com/static/img/weather/png/hr.png","weatherStatus":"Heavy Rain","windDirection":"WSW","windSpeed":15.488408930315757},{"dateTime":"04.10.2017 15:22","funnyAdvice":"Remember your umbrella today!","humidity":83,"predictability":75,"temp":12.3,"weatherIcon":"https://www.metaweather.com/static/img/weather/png/lr.png","weatherStatus":"Light Rain","windDirection":"WSW","windSpeed":15.095879450315755},{"dateTime":"04.10.2017 18:22","funnyAdvice":"Remember your umbrella today!","humidity":83,"predictability":75,"temp":11.21,"weatherIcon":"https://www.metaweather.com/static/img/weather/png/lr.png","weatherStatus":"Light Rain","windDirection":"WSW","windSpeed":15.334485988134892},{"dateTime":"04.10.2017 21:22","funnyAdvice":"Remember your umbrella today!","humidity":84,"predictability":75,"temp":11.21,"weatherIcon":"https://www.metaweather.com/static/img/weather/png/lr.png","weatherStatus":"Light Rain","windDirection":"WSW","windSpeed":14.942485988134894}]
+    */
+    @Test
+    public void testGetRightDate() throws NotFoundException {
+        List<WeatherDTO> res = facade.getWeather("Copenhagen", "2017", "10", "04");
+        assertTrue(res.contains(weatherOne));
+        assertTrue(res.contains(weatherTwo));
     }
 
 }
